10f8aeaafea26d24715691d7ba15a34a91c6d180
Mark Williams <mark@jomint.com>
2017-02-01 15:14:03 -0600

    build testing

--- src/icd/icd.go
+++ src/icd/icd.go
@@ -12,7 +12,17 @@ import (
 type ICDPlugin struct{}
 
 func (c *ICDPlugin) Run(cliConnection plugin.CliConnection, args []string) {
+    var shouldRequest bool = false
+    var method string = "POST"
     if args[0] == "icd" && len(args) > 2 && args[1] == "--create-connection" {
+       shouldRequest = true
+       method = "POST"
+    } else if args[0] == "icd" && len(args) > 2 && args[1] == "--delete-connection" {
+       shouldRequest = true
+       method = "DELETE"
+    }
+
+    if (shouldRequest) {
         webhook_url := args[2]
         appName := args[3]
         fmt.Println("w: %s, a: %s", webhook_url, appName)
@@ -24,17 +34,24 @@ func (c *ICDPlugin) Run(cliConnection plugin.CliConnection, args []string) {
         check(err)
         current_app, err := cliConnection.GetApp(appName)
         check(err)
+        //temporary measure only for demo, use TIAM instead on server side
+        at, err := cliConnection.AccessToken()
+        check(err)
         type Message struct {
             Org plugin_models.Organization
             Space plugin_models.Space
             App plugin_models.GetAppModel
             ApiEndpoint string
+            Method string
+            Token string
         }
         amp := Message {
             Org: current_org,
             Space: current_space,
             App: current_app,
             ApiEndpoint: apiEndpoint,
+            Method: method,
+            Token: at,
         }
         fmt.Println(amp.Org)
         js, err := json.Marshal(amp)
